90f73ad1cb98055bf6958e1abaf102bc62e05b9f
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-01-25 12:56:55 -0500

    sprint in process

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage}/>
+      <Route exact path="/Characters" component={CharacterList}/>
+      <Route exact path="/Search" component={SearchForm}/>
     </main>
   );
 }
